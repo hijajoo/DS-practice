98d729ca06ae825354fdbac49ca354587833093f
hijajoo <hijajoo@users.noreply.github.com>
2017-07-06 17:24:58 +0530

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+  private:
+  int val;
+  Node *next;
+  
+  public:
+  Node(int x=0){
+    val = x;
+    next = NULL;
+  }
+  Node *getNext(){
+    return next;
+  }
+  void setNext(Node *x){
+    next = x;
+  }
+  int getVal(){
+    return val;
+  }
+  void setVal(int x){
+    val = x;
+  }
+  friend ostream &operator <<(ostream out, Node x){
+    return out << x.getVal();
+  }
+};
